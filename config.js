acff9225ad824d2c960117ac7c0daf45e12e686f
Kuroser <creepypastas248@gmail.com>
2021-12-15 20:27:13 +0100

    15/12/2012 20:27

--- config.js
+++ config.js
@@ -1,11 +1,21 @@
-// Firebase Config
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "API_KEY_HERE",
-    authDomain: "AUTH_DOMAIN_HERE",
-    databaseURL: "DATABASE_URL_HERE",
-    projectId: "PROJECT_ID_HERE",
-    storageBucket: "STORAGE_BUCKET_HERE",
-    messagingSenderId: "MESSAGING_ID_HERE"
-  }
-  // Exports
-  module.exports = firebaseConfig;
+  apiKey: "AIzaSyCeAGPR3WyKJ39cjC8QmYNaFhCsMOmAfkM",
+  authDomain: "agapeaangular-b7bb6.firebaseapp.com",
+  projectId: "agapeaangular-b7bb6",
+  storageBucket: "agapeaangular-b7bb6.appspot.com",
+  messagingSenderId: "937841281730",
+  appId: "1:937841281730:web:4a229cae341143636dc95b",
+  measurementId: "G-GD9SV84DZJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
